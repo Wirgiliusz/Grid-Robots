18b0a61aada5a5989bbf460cbf4cba814c85a316
Wirgiliusz <kamx98@gmail.com>
2021-12-06 18:14:04 +0100

    Add reading grid size from input file

--- Controller/main.c
+++ Controller/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define INPUT_BUFFER_SIZE 1024
 
 int main() {
     printf("Hello Controller!\n");
@@ -10,6 +13,16 @@ int main() {
         perror("File opening failed");
         return EXIT_FAILURE;
     }
+
+    char input_line[INPUT_BUFFER_SIZE];
+
+    size_t grid_size_i;
+    size_t grid_size_j;
+    fgets(input_line, INPUT_BUFFER_SIZE, file);
+    char *token = strtok(input_line, " ");
+    grid_size_i = atoi(token);
+    token = strtok(NULL, " ");
+    grid_size_j = atoi(token);
 
 
     return EXIT_SUCCESS;
