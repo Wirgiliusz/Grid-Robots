6f3489630ce64ed508b2df7a2fe7a737c14faac2
Wirgiliusz <kamx98@gmail.com>
2021-12-06 17:10:00 +0100

    Add basic input.txt file

--- Controller/main.c
+++ Controller/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     printf("Hello Controller!\n");
 
-    return 0;
+    FILE *file;
+    file = fopen("input.txt", "r");
+    if (!file) {
+        perror("File opening failed");
+        return EXIT_FAILURE;
+    }
+
+
+    return EXIT_SUCCESS;
 }
