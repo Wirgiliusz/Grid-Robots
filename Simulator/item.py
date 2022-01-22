c1bf878a74f0e5cca80b8fd5c52fe105e275e871
Wirgiliusz <kamx98@gmail.com>
2022-01-22 20:27:36 +0100

    Update items to use only one type of image

--- Simulator/item.py
+++ Simulator/item.py
@@ -4,7 +4,8 @@ from random import randint
 class Item:
     def __init__(self, i, j) -> None:
         images = ('img/item_box.png', 'img/item_circle.png', 'img/item_triangle.png')
-        img_idx = randint(0, 2)
+        #img_idx = randint(0, 2)
+        img_idx = 0 # Override item image for now
         self.image = pygame.image.load(images[img_idx]).convert_alpha()
         self.image = pygame.transform.scale(self.image, (40, 40))
         self.i = i
